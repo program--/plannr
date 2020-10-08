ca613dc75e7b02743b2ea1dae1dac32697978045
program-- <justin@justinsingh.me>
2020-10-07 21:28:12 -0700

    troubleshooting build

--- tests/testthat/helper-plannr.R
+++ tests/testthat/helper-plannr.R
@@ -4,14 +4,14 @@ control_data_read[[1]] <- "Test Plan"
 control_data_read[[2]] <- "10/06/2020"
 control_data_read[[3]] <-
   readr::read_csv(system.file(
-    "inst",
+    "tests",
     "testdata",
     "test_plan.csv",
     package="plannr")
   ) %>%
-  convert(chr(`Start Date`, Late, Description, Labels))
+  hablar::convert(hablar::chr(`Start Date`, Late, Description, Labels))
 control_data_read[[3]][11] <-
-  vapply(control_data_read[[3]][11], tolower)
+  lapply(control_data_read[[3]][11], tolower)
 
 control_data_filter <- control_data_read
 control_data_filter[[3]] <- control_data_filter[[3]] %>%
@@ -20,7 +20,7 @@ control_data_filter[[3]] <- control_data_filter[[3]] %>%
 # Test Data ---------------------------------------------------
 test_data_read <-
   plannr::read_planner(system.file(
-    "inst",
+    "tests",
     "testdata",
     "test_plan.xlsx",
     package="plannr")
